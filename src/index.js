5827b5d73cc66eed2ef238cc023dc3696c8c6595
Prosukov Oleksandr <122283728+ProsukovOleksandr@users.noreply.github.com>
2023-06-22 13:54:27 +0300

    update goit-js-hw-11

--- src/index.js
+++ src/index.js
@@ -10,38 +10,31 @@ loadMoreBtn.classList.add('is-hidden');
 let page;
 let lightbox;
 const limit = 40;
-function createImgMarkup({
-  webformatURL,
-  largeImageURL,
-  tags,
-  likes,
-  views,
-  comments,
-  downloads,
-}) {
-  return  `
-    <div class="photo-card">
-  <a href="${largeImageURL}"><img src="${webformatURL}" alt="${tags} loading="lazy"></a>
-  <div class="info">
-    <p class="info-item">
-      <b>Likes</b>
-      ${likes}
-    </p>
-    <p class="info-item">
-      <b>Views</b>
-      ${views}
-    </p>
-    <p class="info-item">
-      <b>Comments</b>
-      ${comments}
-    </p>
-    <p class="info-item">
-      <b>Downloads</b>
-      ${downloads}
-    </p>
-  </div>
+function createImgMarkup({webformatURL,largeImageURL, tags,likes,views,comments,downloads,}){
+  let html = `
+  <div class="photo-card">
+<a href="${largeImageURL}"><img src="${webformatURL}" alt="${tags} loading="lazy"></a>
+<div class="info">
+  <p class="info-item">
+    <b>Likes</b>
+    ${likes}
+  </p>
+  <p class="info-item">
+    <b>Views</b>
+    ${views}
+  </p>
+  <p class="info-item">
+    <b>Comments</b>
+    ${comments}
+  </p>
+  <p class="info-item">
+    <b>Downloads</b>
+    ${downloads}
+  </p>
 </div>
-  `;
+</div>
+`;
+  return html;
 }
 
 formEl.addEventListener('submit', event => {
